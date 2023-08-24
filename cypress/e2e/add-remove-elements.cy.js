f378a67cdf2165df04a206447a9c9094bdf3b9f6
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-08-24 22:13:25 +0200

    It deletes elements in add remove elements test.

--- cypress/e2e/add-remove-elements.cy.js
+++ cypress/e2e/add-remove-elements.cy.js
@@ -1,15 +1,22 @@
-import homePage from "../pageObjects/home-page.js";
 import testData from "../fixtures/testData.json";
 
 describe("GO TO ADD REMOVE ELEMENTS", () => {
-  before(function () {
+  beforeEach(function () {
     cy.visit("/");
     cy.contains(testData.sectionName.addRemove).click();
   });
 
   it("checks if the user went to add remove elements section", () => {
-    for (let i = 0; i < 4; i++) {
+    cy.get('[onclick="addElement()"]').should("be.visible");
+  });
+
+  it("adds 10 elements and deletes 10 elements", () => {
+    for (let i = 0; i < 10; i++) {
       cy.get('[onclick="addElement()"]').click({ multiple: true });
+    }
+
+    for (let i = 0; i < 10; i++) {
+      cy.get("#elements > :nth-child(1)").click({ multiple: true });
     }
   });
 });
