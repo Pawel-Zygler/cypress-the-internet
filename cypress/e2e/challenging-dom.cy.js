ec864d7abb83be6bfd262952c53b55fb2d62af2f
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-08-24 22:15:43 +0200

    Added challenging dom test. Pics one value from table.

---
+++ cypress/e2e/challenging-dom.cy.js
@@ -0,0 +1,12 @@
+import testData from "../fixtures/testData.json";
+
+describe("CHALLENGING DOM", () => {
+  beforeEach(function () {
+    cy.visit("/");
+    cy.contains(testData.sectionName.challengingDom).click();
+  });
+
+  it("pics the ", () => {
+    cy.get(":nth-child(7) > :nth-child(5)").should("contain", "Consequuntur6");
+  });
+});
