b10a3ed70250977993d8d7c2f9853bf2c34eb5d9
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-08-26 14:06:24 +0200

    'Exit intent' test added.

---
+++ cypress/e2e/exit-intent.cy.js
@@ -0,0 +1,15 @@
+import testData from "../fixtures/testData.json";
+
+describe("EXIT INTENT", () => {
+  beforeEach(function () {
+    cy.visit("/");
+    cy.viewport(1024, 768);
+    cy.contains(testData.sectionName.exit).click();
+  });
+
+  it("closes ad", () => {
+    cy.get("html").trigger("mouseleave", 0, 0);
+    cy.get(".modal-title h3").should("be.visible");
+    cy.get(".modal-footer > p").click();
+  });
+});
