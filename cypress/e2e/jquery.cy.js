838b97608fd910bdd8f346b59f3adb1006617754
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-08-31 21:19:16 +0200

    Added jquery test.

---
+++ cypress/e2e/jquery.cy.js
@@ -0,0 +1,20 @@
+import testData from "../fixtures/testData.json";
+
+describe("JQUERY", () => {
+  beforeEach(function () {
+    cy.visit("/");
+    cy.contains(testData.sectionName.jquery).click();
+  });
+
+  xit("jquery dropdown", () => {
+    cy.get("#ui-id-3 > :nth-child(2)").click();
+    cy.get('#ui-id-4 > [href="#"]').click();
+    cy.get("#ui-id-5 > a").click();
+  });
+
+  it("jquery - with contains", () => {
+    cy.contains("Enabled").click();
+    cy.contains("Downloads").click();
+    cy.contains("PDF").click();
+  });
+});
